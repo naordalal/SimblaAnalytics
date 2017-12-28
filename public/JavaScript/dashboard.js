e4b26c86f9c27fbac27edd1f38a2dbada3bb4574
ohaddali <ohadda@post.bgu.ac.il>
2017-12-28 14:48:03 +0200

    Added list of countries to the dashboard.

--- public/JavaScript/dashboard.js
+++ public/JavaScript/dashboard.js
@@ -1,8 +1,29 @@
 
 
 var es = new EventSource('/visitEvent');
-
-
+var countryList;
+$(document).ready(function() {
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    xhr.open('POST',"",true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function(next) {
+        if (xhr.readyState === 4)
+        {
+            countryList = JSON.parse(xhr.response);
+            var list = document.getElementById('countries');
+            var i;
+            for (i=0 ; i<countryList.length;i++)
+            {
+                var entry = document.createElement('li');
+                entry.appendChild(document.createTextNode(countryList[i].name + ":" + countryList[i].phone));
+                list.appendChild(entry);
+            }
+        }
+    };
+    xhr.send();
+})
+console.log(countryList)
 es.addEventListener('NewVisit', function (event) {
         var data = event.data;
         document.getElementById("numVisits").innerText = "Number of Visits: "+ data;
