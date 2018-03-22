c4b9f83b39ce6f82801b568e244aa04b45a8069b
ohaddali <ohadda@post.bgu.ac.il>
2018-03-22 10:46:14 +0200

    Referrer added

--- public/ClientScript/script.js
+++ public/ClientScript/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
-    xhr.open('POST',"http://132.73.205.75:3000/visit",true); //TODO : Change URL.
+    xhr.open('POST',"http://132.73.211.244:3000/visit",true); //TODO : Change URL.
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
     var siteId = getSiteId();
-
+    var referrer  = document.referrer;
     if(siteId != null)
     {
         var params = "siteId=" + siteId;
+        params += "&referrer="+extractRootDomain(referrer);
         xhr.send(params);
     }
 });
@@ -28,4 +29,42 @@ function getSiteId()
     }
 
     return siteId;
+}
+
+
+function extractHostname(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    }
+    else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+function extractRootDomain(url) {
+    var domain = extractHostname(url),
+        splitArr = domain.split('.'),
+        arrLen = splitArr.length;
+
+    //extracting the root domain here
+    //if there is a subdomain
+    if (arrLen > 2) {
+        domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+        //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+        if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+            //this is using a ccTLD
+            domain = splitArr[arrLen - 3] + '.' + domain;
+        }
+    }
+    return domain;
 }
