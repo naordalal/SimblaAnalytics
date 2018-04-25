fbe933caba17272a55e64db2ffd4083dbffd2719
korenleven <korenleven@gmail.com>
2018-04-25 23:57:05 +0300

    Adding scrolling feature

---
+++ routes/scrolling.js
@@ -0,0 +1,13 @@
+var bigquery = require('../queries');
+
+router.route('/').post(function(req, res, next) {
+    bigquery.insertScrollPercentage(req.body.siteId ,req.body.pageId ,req.body.scroll ,new Date());
+});
+
+module.exports = router;
+
+module.exports.getSitesScrollingPercentage = function (siteId) {
+    return bigquery.getSiteScrollingPercentage(siteId).then(function (result) {
+        return result;
+    })
+};
